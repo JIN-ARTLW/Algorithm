d1024b25e7be4f8c1ea89ac4fb97769228b0123d
JIN <andrewtaylorweir@gmail.com>
2021-09-19 13:06:58 +0900

    20210919

--- Basics/Chap5-arrange1/8958-oxQ.c
+++ Basics/Chap5-arrange1/8958-oxQ.c
@@ -1,9 +1,17 @@
-// #include <stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d", &n);
-//     char a[80];
-//     for(int i=0;i<n;i++){
-//         scanf("%d", )
-//     }
-// }
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int n, output=0, num=0;
+    scanf("%d", &n);
+    char a[80];
+    for(int i=0;i<n;i++){
+        scanf("%s", a);
+        num=0; output=0;
+        for(int j=0;j<strlen(a);j++){
+            if(a[j]=='O') num++;
+            else if(a[j] == 'X') num=0;
+            output+=num;
+        }
+        printf("%d\n", output);
+    }
+}
